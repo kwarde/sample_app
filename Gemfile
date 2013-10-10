9f5472916bd0031872caf48a2a30dd6d0bd767f0
Kevin Warde <kevinwarde@gmail.com>
2013-10-09 23:45:34 -0400

    up to 5.3.2 rails routes

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'newrelic_rpm'
 
 gem 'rails', '4.0.0'
 gem 'pg', '0.15.1'
+gem 'bootstrap-sass', '2.3.2.0'
 
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,12 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
@@ -20,12 +20,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -33,12 +33,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -46,12 +46,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
