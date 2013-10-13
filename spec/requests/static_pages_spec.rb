788e397a8fd84cad3a9dd5cb3dbccce6bdabbf35
Kevin Warde <kevinwarde@gmail.com>
2013-10-13 01:11:41 -0400

    exercises at end of chapt 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,63 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title (full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App'}
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+    # removed below lines for shared_examples method
+    # it { should have_content('Sample App') }
+    # it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help'}
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
+    #it { should have_content('Help') }
+    #it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading) { 'About'}
+    let(:page_title) { 'About Us' }
+    it_should_behave_like "all static pages"
+#    it { should have_content('About') }
+ #   it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact'}
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
+#    it { should have_selector('h1', text: 'Contact') }
+#    it { should have_title(full_title('Contact')) }
+  
+#Insert for link checks
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "Sign up now!"
+    expect(page). to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page). to have_title(full_title(''))
+  end
   end
 end
