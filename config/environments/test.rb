e3a48fea2e9b2569410ce93bf7d71b4f69b98b5e
kwarde <kevinwarde@gmail.com>
2013-10-22 16:56:40 -0400

    up to 7.2.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.configure do
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
