a20192e91093f8641db23486a553fe3072e24b19
kwarde <kevinwarde@gmail.com>
2013-10-28 20:06:34 -0400

    up to 9.2 but w 5 fails in test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,19 +25,18 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+  end
+  
   describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end  
+    
     describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
