3023d197edfa282b4bd6637f75f968c4ee028d84
kwarde <kevinwarde@gmail.com>
2013-11-15 14:24:25 -0500

    up to section 9.3.2 but w one error - line 81 sign in not there

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,11 +31,12 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    
+
     describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
@@ -63,7 +64,7 @@ end
       end
     end
 
-      describe "in the Users controller" do
+    describe "in the Users controller" do
 
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -74,13 +75,12 @@ end
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before  { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
-
-
-
-
-
-
 
   describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
